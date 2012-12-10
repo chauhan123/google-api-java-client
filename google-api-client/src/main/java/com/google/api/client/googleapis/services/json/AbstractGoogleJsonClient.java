dc1481dbd12cc12d785bfc87f708eb32b36a4d17
Ravi Mistry <rmistry@google.com>
2012-12-10 09:17:02 -0500

    api Issue 591: MediaHttpUploader.upload() requires a known InputStreamContent size. api Issue 662: MediaHttpUploader should enforce that chunk sizes are multiples of 256KB. api Issue 663: The range query request in MediaHttpUploader#serverErrorCallback fails with a 411. api Issue 641: MediaHttpUploader: direct upload should set the request headers. api Issue 673: NullPointerException on error when doing resumable media upload. api Issue 638: MediaHttpUploader should respect AbstractGoogleClientRequest.setDisableGZipContent(true).
    https://codereview.appspot.com/6852066/

---
+++ google-api-client/src/main/java/com/google/api/client/googleapis/services/json/AbstractGoogleJsonClient.java
@@ -0,0 +1,163 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.googleapis.services.json;
+
+import com.google.api.client.googleapis.json.JsonCParser;
+import com.google.api.client.googleapis.services.AbstractGoogleClient;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.api.client.googleapis.subscriptions.SubscriptionStore;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+
+/**
+ * Thread-safe Google JSON client.
+ *
+ * @since 1.12
+ * @author Yaniv Inbar
+ */
+public abstract class AbstractGoogleJsonClient extends AbstractGoogleClient {
+
+  /**
+   * @param transport HTTP transport
+   * @param jsonFactory JSON factory
+   * @param rootUrl root URL of the service
+   * @param servicePath service path
+   * @param httpRequestInitializer HTTP request initializer or {@code null} for none
+   * @param legacyDataWrapper whether using the legacy data wrapper in responses
+   */
+  protected AbstractGoogleJsonClient(HttpTransport transport, JsonFactory jsonFactory,
+      String rootUrl, String servicePath, HttpRequestInitializer httpRequestInitializer,
+      boolean legacyDataWrapper) {
+    super(transport, httpRequestInitializer, rootUrl, servicePath, newObjectParser(
+        jsonFactory, legacyDataWrapper));
+  }
+
+  /**
+   * @param transport HTTP transport
+   * @param httpRequestInitializer HTTP request initializer or {@code null} for none
+   * @param rootUrl root URL of the service
+   * @param servicePath service path
+   * @param jsonObjectParser JSON object parser
+   * @param googleClientRequestInitializer Google request initializer or {@code null} for none
+   * @param applicationName application name to be sent in the User-Agent header of requests or
+   *        {@code null} for none
+   * @param suppressPatternChecks whether discovery pattern checks should be suppressed on required
+   *        parameters
+   */
+  protected AbstractGoogleJsonClient(HttpTransport transport,
+      HttpRequestInitializer httpRequestInitializer, String rootUrl, String servicePath,
+      JsonObjectParser jsonObjectParser,
+      GoogleClientRequestInitializer googleClientRequestInitializer, String applicationName,
+      boolean suppressPatternChecks) {
+    super(transport, httpRequestInitializer, rootUrl, servicePath, jsonObjectParser,
+        googleClientRequestInitializer, applicationName, suppressPatternChecks);
+  }
+
+  @Override
+  public JsonObjectParser getObjectParser() {
+    return (JsonObjectParser) super.getObjectParser();
+  }
+
+  /** Returns the JSON Factory. */
+  public final JsonFactory getJsonFactory() {
+    return getObjectParser().getJsonFactory();
+  }
+
+  /**
+   * @param jsonFactory JSON factory
+   * @param legacyDataWrapper whether using the legacy data wrapper in responses
+   */
+  static JsonObjectParser newObjectParser(JsonFactory jsonFactory, boolean legacyDataWrapper) {
+    return legacyDataWrapper ? new JsonCParser(jsonFactory) : new JsonObjectParser(jsonFactory);
+  }
+
+  /**
+   * Builder for {@link AbstractGoogleJsonClient}.
+   *
+   * <p>
+   * Implementation is not thread-safe.
+   * </p>
+   */
+  public abstract static class Builder extends AbstractGoogleClient.Builder {
+
+    /**
+     * @param transport HTTP transport
+     * @param jsonFactory JSON factory
+     * @param rootUrl root URL of the service
+     * @param servicePath service path
+     * @param httpRequestInitializer HTTP request initializer or {@code null} for none
+     * @param legacyDataWrapper whether using the legacy data wrapper in responses
+     */
+    protected Builder(HttpTransport transport, JsonFactory jsonFactory, String rootUrl,
+        String servicePath, HttpRequestInitializer httpRequestInitializer,
+        boolean legacyDataWrapper) {
+      super(transport, rootUrl, servicePath, newObjectParser(jsonFactory, legacyDataWrapper),
+          httpRequestInitializer);
+    }
+
+    @Override
+    public final JsonObjectParser getObjectParser() {
+      return (JsonObjectParser) super.getObjectParser();
+    }
+
+    /** Returns the JSON Factory. */
+    public final JsonFactory getJsonFactory() {
+      return getObjectParser().getJsonFactory();
+    }
+
+    @Override
+    public abstract AbstractGoogleJsonClient build();
+
+    @Override
+    public Builder setRootUrl(String rootUrl) {
+      return (Builder) super.setRootUrl(rootUrl);
+    }
+
+    @Override
+    public Builder setServicePath(String servicePath) {
+      return (Builder) super.setServicePath(servicePath);
+    }
+
+    @Override
+    public Builder setGoogleClientRequestInitializer(
+        GoogleClientRequestInitializer googleClientRequestInitializer) {
+      return (Builder) super.setGoogleClientRequestInitializer(googleClientRequestInitializer);
+    }
+
+    @Override
+    public Builder setHttpRequestInitializer(HttpRequestInitializer httpRequestInitializer) {
+      return (Builder) super.setHttpRequestInitializer(httpRequestInitializer);
+    }
+
+    @Override
+    public Builder setApplicationName(String applicationName) {
+      return (Builder) super.setApplicationName(applicationName);
+    }
+
+    /**
+     * @since 1.13
+     */
+    @Override
+    public Builder setSubscriptionStore(SubscriptionStore subscriptionStore) {
+      return (Builder) super.setSubscriptionStore(subscriptionStore);
+    }
+
+    @Override
+    public Builder setSuppressPatternChecks(boolean suppressPatternChecks) {
+      return (Builder) super.setSuppressPatternChecks(suppressPatternChecks);
+    }
+
+  }
+}
